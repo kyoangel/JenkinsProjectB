0574c12e8e75a83293d803ba164925df7a296c0e
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-07-29 19:02:36 +0800

    add test proj

---
+++ ProjectBTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectB.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+        }
+    }
+}
